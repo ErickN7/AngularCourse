894545b00c69e15a3caf55b954d9067cfed8ec07
Kubo Developer 7 <kubodeveloper7@outlook.com>
2023-03-09 17:46:52 -0600

    Cambios

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -1,8 +1,14 @@
 class Heroe{
-    alterEgo: string;
-    edad: number;
-    nombreReal: string;
+    // alterEgo: string;
+    // edad: number;
+    // nombreReal: string;
+
+    constructor(
+        public alterEgo: string,
+        public edad: number,
+        public nombreReal: string
+    ){}
 }
 
-const ironman = new Heroe();
+const ironman = new Heroe('Ironman', 47, 'Tony');
 console.log(ironman);
